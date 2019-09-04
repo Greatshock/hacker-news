e70695dc3028dece5d42f144685d36b4ef0509eb
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2019-09-05 00:19:02 +0300

    Start

--- app/index.js
+++ app/index.js
@@ -1,7 +1,34 @@
 import '../assets/index.css';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends React.Component {
+    state = {
+        theme: 'light',
+        toggleTheme: () => {
+            this.setState(({theme}) => ({
+                theme: theme === 'light' ? 'dark' : 'light'
+            }))
+        }
+    };
 
+    render() {
+        return (
+            <Router>
+                <div className={this.state.theme}>
+                    <Route exact
+                           path="/"
+                           render={() => 'Top'}>
+                    </Route>
+                </div>
+            </Router>
+        );
+    }
 }
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
